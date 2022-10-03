402fc66fad3166b38ce8ed069e7ecccc5588f64d
Dzianis Bohush <110992506+densparrow@users.noreply.github.com>
2022-10-03 22:08:24 +0300

    Создал файл main.css, contact.html.

--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name" : "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "fist-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 @app.route('/')
-@app.route('/home')
 def index():
+    print( url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route('/about')
 def about():
+    print( url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
 
+@app.route('/contact')
+def contact():
+    print( url_for('contact'))
+    return render_template('contact.html', title='Обратная связь', menu=menu)
 
 @app.route('/user/<string:name>/<int:id>')
 def user(name, id):
     return "User page" + name + '-' + id
 
+
+@app.route('/profile/<username>')
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+# with app.test_request_context(): # тестовый контекст запрос
+#     print( url_for('index'))
+#     print( url_for('about'))
+#     print( url_for('profile', username='denis'))
 
 if __name__ == '__main__':
     app.run(debug=True)
